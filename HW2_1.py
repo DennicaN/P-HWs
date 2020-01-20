dc905980f6385456ed62072a4c139b2aea90e613
DennicaN <58238184+DennicaN@users.noreply.github.com>
2020-01-21 01:16:19 +0500

    Create HW2_1.py

---
+++ HW2_1.py
@@ -0,0 +1,8 @@
+result_list = [2, 'text', 45.3, None]
+#print(type(result_list[0]))
+#print(type(result_list[1]))
+#print(type(result_list[2]))
+#print(type(result_list[3]))
+
+for i in result_list:
+    print(f'{i} is {type(i)}')
