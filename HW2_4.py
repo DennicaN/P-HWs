8c5aceaceb93aa01f97340ebc3600df3068fa5b3
DennicaN <58238184+DennicaN@users.noreply.github.com>
2020-01-21 15:05:57 +0500

    Update HW2_4.py

--- HW2_4.py
+++ HW2_4.py
@@ -5,7 +5,7 @@ for el in range(user_string.count(" ") + 1):
     words = user_string.split()
     if len(str(words)) <= 10:
         print(f'{num}, {words [el]}')
-        num = num + 1
+        num += 1
     else:
         print(f'{num}, {words [el] [:10]}')
-        num = num + 1
+        num += 1
