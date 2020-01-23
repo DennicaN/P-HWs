2b2d62917527cbf8cc4deb3dbfebea5dde9c038c
DennicaN <58238184+DennicaN@users.noreply.github.com>
2020-01-24 01:10:23 +0500

    Create HW3_6.py

---
+++ HW3_6.py
@@ -0,0 +1,5 @@
+def int_func(*args):
+    word = str(input('Укажите слово в нижнем регистре (все буквы маленькие) или строку текса в нижнем регистре (задание 2)'))
+    print(word.title())
+    return
+int_func()
