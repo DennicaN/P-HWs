e8b9e61c2fe5c92655edeb6927f8bed052893cce
DennicaN <58238184+DennicaN@users.noreply.github.com>
2020-01-24 01:40:19 +0500

    Create HW3_4.py

---
+++ HW3_4.py
@@ -0,0 +1,16 @@
+def my_func(x, y):
+    return x ** y
+
+print(my_func(2, -5))
+
+
+def my_func(x, y):
+       x = 2
+       y = -5
+       result = 0
+       step = 0
+       while step <= (y):
+           result = pow(x, 2)
+           step += 1
+    return item
+print(my_func())
